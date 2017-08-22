12d3c6cb9e2b85d9efd46409694773db5a70673b
815109484 <815109484>
2017-08-22 13:43:33 +0800

    Initial commit

---
+++ app/src/main/java/com/fjdy/testgithub/MainActivity.java
@@ -0,0 +1,19 @@
+package com.fjdy.testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a=0;
+        System.out.print("aaaaa");
+        System.out.print("aaaaa");
+
+
+        //222
+    }
+}
